3dd597a73e632b4014ef0d94825b4745a0bd8128
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-08-23 16:41:56 +0530

    update genPassword.js

--- src/functions/misc/genPassword.js
+++ src/functions/misc/genPassword.js
@@ -1,6 +1,6 @@
 module.exports = async function (num) {
 	const rile = num.toString()
-	const { gen } = require("../custom/genPassword")
+	const { gen } = require("../../custom/genPassword")
 if (isNaN(rile)) return "Please provide a valid number!" 
 if (rile.includes(".")) return "Decimal numbers are not allowed"
 if (rile < 1) return "Provide a number greater than 1"
